6060659bc52e6ac88513d2da2245eaf0002ea398
CHPrado <christianprado.contato@gmail.com>
2021-01-16 02:17:12 -0300

    Triplex game

---
+++ TripleX.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <ctime>
+
+void PrintIntroduction(int Difficulty) {
+  std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+  std::cout << " secure server room...\nEnter the correct code to continue...\n\n";
+}
+
+bool PlayGame(int Difficulty) {
+  PrintIntroduction(Difficulty);
+
+  const int CodeA = rand() % Difficulty + Difficulty;
+  const int CodeB = rand() % Difficulty + Difficulty;
+  const int CodeC = rand() % Difficulty + Difficulty;
+
+  const int CodeSum = CodeA + CodeB + CodeC;
+  const int CodeProduct = CodeA * CodeB * CodeC;
+
+  std::cout << "There are 3 numbers in the code\n";
+  std::cout << "\nThe codes add-up to: " << CodeSum;
+  std::cout << "\nThe codes multiply to give: " << CodeProduct << std::endl;
+
+  int GuessA, GuessB, GuessC;
+
+  std::cin >> GuessA >> GuessB >> GuessC;
+
+  const int GuessSum = GuessA + GuessB + GuessC;
+  const int GuessProduct = GuessA * GuessB * GuessC;
+
+  if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+    std::cout << "\n*** Well done agent! You have extracted a file! Keep going! ***";
+    return true;
+  }
+  else {
+    std::cout << "\n *** You entered the wrong code! Careful agent! Try again! ***";
+    return false;
+  }
+}
+
+int main() {
+  srand(time(NULL));
+
+  int LevelDifficulty = 1;
+  const int MaxDifficulty = 5;
+
+  while (LevelDifficulty <= MaxDifficulty) {
+    bool bLevelComplete = PlayGame(LevelDifficulty);
+    std::cin.clear();
+    std::cin.ignore();
+
+    if (bLevelComplete) {
+      ++LevelDifficulty;
+    }
+  }
+
+  std::cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
+
+  return 0;
+}
